10330901784bf8ad5b4ab0215a66c206f0c1c022
dirm <anton.kerke@gmail.com>
2022-02-02 06:31:15 +0200

    stylized textbox

--- MusicLibraryOrganizer.py
+++ MusicLibraryOrganizer.py
@@ -6,10 +6,12 @@ import os
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+import tkinter.font as tkf
 from mutagen.flac import FLAC
 from mutagen.mp3 import EasyMP3 as MP3
 import threading
 import time
+import string
 
 # Global variables
 supportedFiletypes = [".flac", ".mp3"]
@@ -34,9 +36,22 @@ class Track:
         self.artist = artist
         self.album = album
 
-    def formatMetadata(self):
-        # Return formatted metadata.
-        print('formatted metadata.')
+    def removeIllegal(self):
+        # Trans table...i dunno, still cant be arsed to read documentation
+        remove = r'"\/:?*<>|'
+        # Remove remove illegal
+        # Tracknumber
+        table = self.tracknumber.maketrans('', '', remove)
+        self.tracknumber = self.tracknumber.translate(table)
+        # Artist
+        table = self.artist.maketrans('', '', remove)
+        self.artist = self.artist.translate(table)
+        # Album
+        table = self.album.maketrans('', '', remove)
+        self.album = self.album.translate(table)
+        # Title
+        table = self.title.maketrans('', '', remove)
+        self.title = self.title.translate(table)
 
     # Return new filename based on metadata. RETURNS FILENAME AS STRING
     def createNewFilename(self, separator, format):
@@ -52,11 +67,54 @@ class Track:
             newFilename = newFilename.replace('{title}', self.title)
         return newFilename
 
+    def formatTracknumber(self):
+        if len(self.tracknumber) < 2:
+            self.tracknumber = '0' + self.tracknumber
+        # IF TRACKNUMBERING SCUFFS, THIS IS PROBLEM! MAKE LOOP
+        if len(self.tracknumber) > 2:
+            self.tracknumber = self.tracknumber.lstrip('0')
+
+    def formatRestMeta(self, format):
+        if '{artist}' in format:
+            self.artist = string.capwords(self.artist)
+        if '{album}' in format:
+            self.album = string.capwords(self.album)
+            try:
+                index = self.album.index('[')
+                self.album = self.album[:index+1] + self.album[index+1].upper() + self.album[index+2:]
+            except ValueError:
+                pass
+            try:
+                index = self.album.index('(')
+                self.album = self.album[:index+1] + self.album[index+1].upper() + self.album[index+2:]
+            except ValueError:
+                pass
+        if '{title}' in format:
+            self.title = string.capwords(self.title)
+            try:
+                index = self.title.index('[')
+                self.title = self.title[:index+1] + self.title[index+1].upper() + self.title[index+2:]
+            except ValueError:
+                pass
+            try:
+                index = self.title.index('(')
+                self.title = self.title[:index+1] + self.title[index+1].upper() + self.title[index+2:]
+            except ValueError:
+                pass
+
 
 # Renaming
 def renameFiles():
     # Iterate through all the tracks (objects)
     for track in trackList:
+        # formatMetadata(track, entryFormat.get())
+        # Format tracknumbers
+        track.formatTracknumber()
+        # Format rest of metadata
+        track.formatRestMeta(entryFormat.get())
+        # Remove illegal symbols from tracknumber, artist, album and title
+        track.removeIllegal()
+        # Create the filename
         filename = track.createNewFilename(userSeparator.get(), entryFormat.get())
         print(filename)
 
@@ -73,7 +131,7 @@ def addFiles():
     # Start time.
     startTime = time.perf_counter()
     # Set status text
-    statusText.set('Adding files...')
+    statusText.set(' Adding files...')
     # Get current directory
     current_dir = pathlib.Path(folderPath.get())
     # Run the program. Create Track obj for every .flac/.mp3 file.
@@ -92,12 +150,12 @@ def addFiles():
                 trackList.append(Track(path, ext, metadata['title'][0], metadata['tracknumber'][0], metadata['artist'][0], metadata['album'][0]))
                 # add to added counter.
                 added += 1
-                programOutput(str(path) + '   --> Added')
+                programOutput('  ' + str(path), 'added')
             except KeyError:
                 # add to error and skipped counters
                 errors += 1
                 skipped += 1
-                programOutput(str(path) + '   --> ERROR: Failed to Add. Missing metadata?')
+                programOutput('  ' + str(path) + ' - Error: Failed to Add. Missing metadata?', 'error')
         # If ext is .mp3
         elif ext == supportedFiletypes[1]:
             # same as flac section, check comments there.
@@ -106,36 +164,36 @@ def addFiles():
                 trackList.append(Track(path, ext, metadata['title'][0], metadata['tracknumber'][0], metadata['artist'][0], metadata['album'][0]))
                 # add to added counter.
                 added += 1
-                programOutput(str(path) + '   --> Added')
+                programOutput('  ' + str(path), 'added')
             except KeyError:
                 # add to error and skipped counters.
                 errors += 1
                 skipped += 1
-                programOutput(str(path) + '   --> ERROR: Failed to Add. Missing metadata?')
+                programOutput('  ' + str(path) + ' - Error: Failed to Add. Missing metadata?', 'error')
         else:
             # Check if path is a folder
             if os.path.isdir(path) is True:
                 # add to folder counter, remove one from file counter.
                 folders += 1
                 files -= 1
-                programOutput(str(path))
+                programOutput(str(path), 'folder')
             else:
                 skipped += 1
-                programOutput(str(path) + '   --> Skipped')
+                programOutput('  ' + str(path), 'skipped')
     # End time.
     endTime = time.perf_counter()
     # Update status text
     # ADD STATISTICS HERE LATER (RUNTIME, FOLDERS, ADDED FILES, SKIPPED FILES)
-    statusText.set(f'Folders: {folders}  Files: {files}  Added: {added}  Skipped: {skipped}  Errors: {errors}  Time: {endTime - startTime:0.2f}s')
+    statusText.set(f' Folders: {folders}  Files: {files}  Added: {added}  Skipped: {skipped}  Errors: {errors}  Time: {endTime - startTime:0.2f}s')
     # Check if tracklist has items
     if not len(trackList) == 0:
         btnRename.configure(state="normal")
 
 
 # Insert text into program window.
-def programOutput(text):
+def programOutput(text, tags=None):
     textbox.configure(state="normal")
-    textbox.insert(tk.END, " " + text + "\n")
+    textbox.insert(tk.END, ' ' + text + '\n', tags)
     textbox.yview(tk.END)
     textbox.configure(state="disabled")
 
@@ -213,9 +271,13 @@ root.geometry('1280x720')
 root.title("Music Library Organizer")
 root.iconbitmap(default="mlo_icon.ico")
 
+# Fonts
+fontBold = tkf.Font(family='Arial', size=10, weight='bold')
+fontNormal = tkf.Font(family='Arial', size=10, weight='normal')
+
 # Create & Configure frames
 statusText = tk.StringVar()
-statusbar = tk.Label(root, textvariable=statusText, bd=1, relief=tk.SUNKEN, anchor=tk.W)
+statusbar = tk.Label(root, textvariable=statusText, font=fontNormal, bd=1, relief=tk.SUNKEN, anchor=tk.W)
 statusbar.pack(side=tk.BOTTOM, fill=tk.X, padx=(4, 4), pady=(1, 4))
 
 sideFrame = tk.Frame(root)
@@ -231,7 +293,7 @@ bottomFrame = tk.Frame(root)
 bottomFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES, padx=(4, 4), pady=(0, 4))
 
 folderPath = tk.StringVar()
-txtField = tk.Entry(topFrame, textvariable=folderPath)
+txtField = tk.Entry(topFrame, font=fontNormal, textvariable=folderPath)
 txtField.pack(side=tk.LEFT, fill=tk.X, expand=tk.YES, padx=(0, 0), pady=(0, 0))
 
 # Browse button.
@@ -248,36 +310,39 @@ checkBoxStates.append(chk('chkAlbum', chkVarAlb))
 chkVarTit = tk.BooleanVar()
 checkBoxStates.append(chk('chkTitle', chkVarTit))
 
-incLabel = tk.Label(sideFrame, text="Include", font="Arial 10 bold")
+incLabel = tk.Label(sideFrame, text="Include", font=fontBold)
 incLabel.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
-checkBox1 = tk.Checkbutton(sideFrame, text="Tracknumber", variable=chkVarNum,
+checkBox1 = tk.Checkbutton(sideFrame, text="Tracknumber", font=fontNormal, variable=chkVarNum,
                            command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
 checkBox1.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
-checkBox2 = tk.Checkbutton(sideFrame, text="Artist", variable=chkVarArt, command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
+checkBox2 = tk.Checkbutton(sideFrame, text="Artist", font=fontNormal, variable=chkVarArt,
+                           command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
 checkBox2.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
-checkBox3 = tk.Checkbutton(sideFrame, text="Album", variable=chkVarAlb, command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
+checkBox3 = tk.Checkbutton(sideFrame, text="Album", font=fontNormal, variable=chkVarAlb,
+                           command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
 checkBox3.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
-checkBox4 = tk.Checkbutton(sideFrame, text="Title", variable=chkVarTit, command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
+checkBox4 = tk.Checkbutton(sideFrame, text="Title", font=fontNormal, variable=chkVarTit,
+                           command=lambda: [toggleFormat(), dynamicFormatString(userSeparator.get())])
 checkBox4.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
 # Language checkboxes
-langLabel = ttk.Label(sideFrame, text="Language", font="Arial 10 bold")
+langLabel = ttk.Label(sideFrame, text="Language", font=fontBold)
 langLabel.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
 checkVarLANG = tk.IntVar()
-checkBoxLANG = tk.Checkbutton(sideFrame, text="Romanji", variable=checkVarLANG, state=tk.DISABLED)
+checkBoxLANG = tk.Checkbutton(sideFrame, text="Romanji", font=fontNormal, variable=checkVarLANG, state=tk.DISABLED)
 checkBoxLANG.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
 # Extra function checkboxes
-extraLabel = ttk.Label(sideFrame, text="Extra", font="Arial 10 bold")
+extraLabel = ttk.Label(sideFrame, text="Extra", font=fontBold)
 extraLabel.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
 checkVar5 = tk.IntVar()
-checkBox5 = tk.Checkbutton(sideFrame, text="Fix Title", variable=checkVar5)
+checkBox5 = tk.Checkbutton(sideFrame, text="Fix Title", font=fontNormal, variable=checkVar5)
 checkBox5.pack(anchor=tk.W, side=tk.TOP, padx=(0, 0))
 
 # Scan folders and add files.
@@ -290,12 +355,12 @@ btnRename.pack(side=tk.LEFT, padx=(2, 1))
 
 # Free naming format
 entryFormat = tk.StringVar(root, value='{tracknumber} - {artist} - {album} - {title}')
-renameFormat = tk.Entry(middleFrame, textvariable=entryFormat)
+renameFormat = tk.Entry(middleFrame, textvariable=entryFormat, font=fontNormal,)
 renameFormat.pack(side=tk.RIGHT, fill=tk.X, expand=tk.YES, padx=(0, 0), pady=(0, 2))
 
 # Separator entry
 userSeparator = tk.StringVar(root, value=' - ')
-separatorEntry = tk.Entry(middleFrame, textvariable=userSeparator, width=3, justify=tk.CENTER)
+separatorEntry = tk.Entry(middleFrame, textvariable=userSeparator, font=fontNormal, width=3, justify=tk.CENTER)
 separatorEntry.pack(side=tk.LEFT, expand=tk.NO, padx=(0, 5), pady=(0, 2))
 
 
@@ -308,6 +373,12 @@ folderPath.trace_add("write", toggleState)
 # textbox
 textbox = tk.Text(bottomFrame, state="disabled")
 textbox.pack(side=tk.TOP, fill=tk.BOTH, expand=tk.YES, padx=(0, 0))
+
+# Textbox tags
+textbox.tag_configure('folder', foreground='black', font=fontBold)
+textbox.tag_configure('error', foreground='red', font=fontNormal)
+textbox.tag_configure('skipped', foreground='grey', font=fontNormal)
+textbox.tag_configure('added', foreground='green', font=fontNormal)
 
 # Mainloop
 root.mainloop()
